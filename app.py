f504116d70118ab5fd4a8e38fc83a120fd0f1047
이진석 <ljs@whisker.kr>
2025-04-23 15:43:03 +0900

    promenetheus에서의 인식 404 문제

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def predict_product_recommend():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
