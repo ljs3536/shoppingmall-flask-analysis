561d0904778a1ec108c84ebfe3ef29ac1ac2b46f
ljs3536 <wlstjr320@naver.com>
2025-07-04 08:41:05 +0900

    helm으로 받은 Kafka 적용을 위한 변경

--- config.py
+++ config.py
@@ -2,7 +2,11 @@ import os
 
 class Config:
     ELASTICSEARCH_URI = os.getenv('ELASTICSEARCH_URI', 'http://localhost:9200')
-    KAFKA_BOOTSTRAP_SERVERS = ['kafka:9092']
+    KAFKA_BOOTSTRAP_SERVERS = [
+        'kafka-controller-0.kafka-controller-headless.default.svc.cluster.local:9092',
+        'kafka-controller-1.kafka-controller-headless.default.svc.cluster.local:9092',
+        'kafka-controller-2.kafka-controller-headless.default.svc.cluster.local:9092'
+    ]
     KAFKA_CONSUMER_GROUP_ID = 'flask-group-test-01'
     KAFKA_TOPIC = 'model-train-topic'
     KAFKA_RESULT_TOPIC = 'model-train-result'
